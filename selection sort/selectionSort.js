7dd99d413e1d179547171b8d15292fb86898363a
Oleh <oleh.fylypiv@gmail.com>
2017-02-22 11:48:42 +0200

    selection sort (#1)

---
+++ selection sort/selectionSort.js
@@ -0,0 +1,26 @@
+function selectionSort(arr){
+	let len = arr.length;
+	for (let i = 0; i<len; i++) {
+		let posMin = i;
+		for (let j = i; j<len; j++) {
+			if(arr[j] < arr[posMin]) {
+				posMin = j;
+			}
+		}
+	
+	 	let temp = arr[posMin];
+	 	arr[posMin] = arr[i];
+	 	arr[i] = temp;
+	}
+}
+
+let arr = [];
+var lenght = prompt('Enter lenght of array');
+    for (let k = 0; k<lenght; k++){
+        let unsortedArr = [prompt('Enter element #'+k)];
+          arr[k] = +unsortedArr;
+    }
+
+alert(arr + ' Click Enter and a program will sort array');
+	selectionSort(arr);
+alert(arr);
